81ce288899fcf6edcea07b16539570673f79b4a5
Nickgiresse <kamdemnick12@gmail.com>
2025-07-19 19:37:03 +0100

    probleme a la page login lors du deploiment

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,7 +5,14 @@ import { Login2 } from "@/components/login2"
 export default function LoginPage() {
   return (
       <div>
-            <Login2 />
+           <div>
+            <Login2 
+              logo={{
+                url: "/",
+                src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-wordmark.svg",
+                alt: "Reachdem logo",
+                title: "Reachdem"
+              }} />
       </div>   
     
   )
