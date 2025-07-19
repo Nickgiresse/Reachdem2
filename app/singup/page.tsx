f3c4e300d5e958c9052d2cdc6f5216f8e35f54c0
Nickgiresse <kamdemnick12@gmail.com>
2025-07-19 19:58:55 +0100

    Update page.tsx

--- app/singup/page.tsx
+++ app/singup/page.tsx
@@ -5,7 +5,14 @@ import { Signup1 } from "@/components/signup1";
 export default function Singup(){
     return(
         <div>
-            <Signup1 />
+            <Signup1 
+                logo={{
+                  url: "/",
+                  src: "https://deifkwefumgah.cloudfront.net/shadcnblocks/block/logos/shadcnblockscom-wordmark.svg",
+                  alt: "Reachdem logo",
+                  title: "Reachdem"
+                }}
+              />
         </div>
     )
 }
